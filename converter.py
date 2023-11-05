d894b6ecad4a66d23bb913289437a4fbc4951e42
number42net <github@number42.net>
2023-11-05 08:22:40 +0100

    Add integer conversions

--- converter.py
+++ converter.py
@@ -3,20 +3,98 @@ esc = bytes.fromhex("1b")
 
 class Converter:
     def IN(self, args):
+        # Manual page 95
         return esc + ".(".encode("ASCII")
 
     def IP(self, args):
+        # Manual pages 95, 100
         raise NotImplementedError("No clue how to scale yet, ignoring...")
 
     def SP(self, args):
-        if not args.isnumeric():
-            raise TypeError("Expect an integer as pen number")
-        if int(args) > 63:
-            raise AttributeError("Expected an integer below 64")
-
-        args = int(args)
-
+        # Manual page 94
         return b"v" + self._covert_to_sbn(args).to_bytes()
 
+    def PD(self, args):
+        # Manual page 106
+        pass
+
     def _covert_to_sbn(self, number: int) -> int:
+        # Manual page 87
+        if not number.isnumeric():
+            raise TypeError("Expect an integer")
+        if int(number) > 63:
+            raise AttributeError("Expected an integer below 64")
+        
+        number = int(number)
+
+        # SBN is a 7 bit integer and the most significant bit is always a 1
         return number | 64
+
+    def _convert_to_mbn(self, number: int) -> bytes:
+        # Manual page 87
+        # Ported from: https://github.com/Toranktto/bsd-plotutils/blob/master/libplot/drivers/hp7221/subr.c
+        
+        if not number.isnumeric():
+            raise TypeError("Expect an integer")
+        if int(number) > 32767:
+            raise AttributeError("Expected an integer below 32768")
+        
+        number = int(number)
+
+        output = b''
+
+        chr = (number >> 12) & 0o7
+        chr |= 0o140
+        output += chr.to_bytes()
+        
+        chr = (number >> 6) & 0o77
+        if chr < 32:
+            chr += 64
+        output += chr.to_bytes()
+        
+        chr = number & 0o77
+        if chr < 32:
+            chr += 64
+        output += chr.to_bytes()
+        
+        return output
+
+    def _convert_to_mbp(self, x: int, y: int) -> bytes:
+        # Manual page 88
+        # Ported from: https://github.com/Toranktto/bsd-plotutils/blob/master/libplot/drivers/hp7221/subr.c
+
+        if not x.isnumeric() and y.x.isnumeric():
+            raise TypeError("Expect an integer")
+        if int(x) > 32767 and int(y) > 32767:
+            raise AttributeError("Expected an integer below 32768")
+
+        x = int(x)
+        y = int(y)
+
+        chr = (x >> 10) & 0o17
+        chr |= 0o140
+        output += chr.to_bytes()
+
+        chr = (x >> 4) & 0o77
+        if chr < 32:
+            chr += 64
+        output += chr.to_bytes()
+
+        chr = (y >> 12) & 0o3
+        chr |= (x << 2) & 0o71
+        if chr < 32:
+            chr += 64
+        output += chr.to_bytes()
+
+        chr = (y >> 6) & 0o77
+        if chr < 32:
+            chr += 64
+        output += chr.to_bytes()
+
+        chr = (y)&0o77
+        if (chr < 32):
+            chr += 64
+        output += chr.to_bytes()
+
+        return output
+
